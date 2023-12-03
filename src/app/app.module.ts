1d3fff6cd792c41a38bd82a3f420552d8ba12a7a
Hryhorii Avcharov <h.avcharov@danieli.com>
2023-12-03 15:31:00 +0100

    Migration Welcome and shared modules +styles +i18n

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
-    CoreModule
-  ],
+    HttpClientModule,
+    CoreModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: (http: HttpClient) =>
+          new TranslateHttpLoader(http, './assets/i18n/', `.json?v=${new Date().getTime()}`),
+        deps: [HttpClient],
+      },
+    })  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { components } from '.';
+import { TranslateModule } from '@ngx-translate/core';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule],
+  imports: [
+    CommonModule, 
+    RouterModule,
+    TranslateModule.forChild({ extend: true }),
+    SharedModule
+  ],
   declarations: [...components],
   exports: [...components],
 })
-export class CoreModule {}
+export class CoreModule { }
